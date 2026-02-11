5e9d94219990831515e1a69569c8dde3577c0056
qinhao <82450473@qq.com>
2026-02-11 16:26:04 +0800

    feat: htmlwebpackplugin可以生成html,然后引入生成的bundle

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,14 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [new HtmlWebpackPlugin({ title: "管理输出" })],
 };
