9e54413f7b84517f0e5bbaf5f62bfe0c78f80e0f
qinhao <82450473@qq.com>
2026-02-11 15:52:17 +0800

    feat: 使用css-loader和style-loader来处理css,css-loader是用来解析css的，style-loader是用来插入到style标签中，css压缩？

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,4 +1,5 @@
 import _ from "lodash";
+import "./style.css";
 
 function component() {
   const element = document.createElement("div");
@@ -6,7 +7,7 @@ function component() {
   // 执行这一行需要引入 lodash（目前通过 script 脚本引入）
   // lodash 现在使用 import 引入
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-
+  element.classList.add("hello");
   return element;
 }
 
