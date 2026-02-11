23b74e9e0fd711d92281b342d998b22195607b6a
qinhao <82450473@qq.com>
2026-02-11 16:43:56 +0800

    feat：webpack-dev-server

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,15 +1,30 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 const path = require("path");
+const { stat } = require("fs");
 
 module.exports = {
+  mode: "development",
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
   },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  plugins: [new HtmlWebpackPlugin({ title: "管理输出" })],
+  optimization: {
+    runtimeChunk: "single",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ title: "Development" }),
+    new WebpackManifestPlugin({
+      fileName: "manifest.json",
+    }),
+  ],
 };
---
+++ webpack.md
@@ -0,0 +1,4 @@
+webpack 通过 manifest 追踪所有模块到输出的 bundle 之间的映射。了解 manifest 将会帮助了解如何以其他方式控制 webpack 输出。
+watch模式需要手动刷新浏览器
+webpack-dev-server能够实时重新加载
+webpack-dev-middleware,可以把webpack处理过的文件发送到server,配合Express
